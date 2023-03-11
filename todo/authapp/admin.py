f1d195704e7160b22c996882d4a510c4cfcc9421
Yuliya Bryukhovskikh <makutu@ereb.ru>
2023-03-11 23:11:41 +0100

    Добавила url и админку пользователя

--- todo/authapp/admin.py
+++ todo/authapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+@admin.register(models.CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ["id", "username", "email", "is_active", "date_joined"]
+    ordering = ["-date_joined"]
